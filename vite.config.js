05b06fb7133912d980f3ed81f8805e6ad0374a52
NGA2005 <noumbissieaudrey3@gmail.com>
2026-04-30 20:06:52 +0100

    fix:passage al'installation de la version 3 de tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Importe le plugin
+  // 1. Importe le plugin
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Ajoute le plugin ici
+     // 2. Ajoute le plugin ici
   ],
 })
