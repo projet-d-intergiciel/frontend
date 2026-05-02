92583f1840a932ecc3b36d9c962ecf743401534a
Carnelle-Enga <carnelleenga@gmail.com>
2026-05-02 13:18:01 +0100

    merging with product-service

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import ProductCatalog from './components/products/ProductCatalog';
+// src/App.jsx
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { StockProvider } from './context/StockContext';
+import Layout from './components/layout/Layout';
+import Login from './pages/Login/Login';
+import DashboardPage from './pages/Dashboard/DashboardPage';
+import StockPage from './pages/Stock/StockPage';
+import OrderListPage from './pages/orders/OrderListPage';
+import NewOrderPage from './pages/orders/NewOrderPage';
+import OrderDetailPage from './pages/orders/OrderDetailPage';
+import UsersPage from './pages/Users/UsersPage';
+import PrivateRoute from './guards/PrivateRoute';
+import RoleGuard from './guards/RoleGuard';
+import ProfilePage from './pages/Profile/ProfilePage';
+import ChangePasswordPage from './pages/Security/ChangePasswordPage';
+import ProductCatalog from './components/products/ProductCatalog'
 
 function App() {
-  return <ProductCatalog />;
   return (
     <BrowserRouter>
       <StockProvider>
@@ -74,12 +88,33 @@ function App() {
                   </RoleGuard>
             </PrivateRoute> 
               } />
-
-            <Route path="/orders/:id" element={
+              
+            <Route path="/orders/new" element={
+              <PrivateRoute>
+                <RoleGuard allowedRoles={['ADMIN', 'GESTIONNAIRE']}>
+                    <Layout>
+                      <NewOrderPage />
+                    </Layout>
+                  </RoleGuard>
+            </PrivateRoute> 
+              } />
+            <Route path="/orders/:id"  element={
               <PrivateRoute>
                 <RoleGuard allowedRoles={['ADMIN', 'GESTIONNAIRE']}>
                     <Layout>
                       <OrderDetailPage />
+                    </Layout>
+                  </RoleGuard>
+            </PrivateRoute> 
+              } />
+
+              {/* route produits */}
+
+            <Route path="/produits" element={
+              <PrivateRoute>
+                <RoleGuard allowedRoles={['ADMIN', 'GESTIONNAIRE']}>
+                    <Layout>
+                      <ProductCatalog />
                     </Layout>
                   </RoleGuard>
             </PrivateRoute> 
